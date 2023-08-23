2d27dfe450f48ca4028d969ff0c988d0eabecf99
Erick Santos <erick.santos@planium.io>
2023-08-23 18:37:59 -0300

    feat(toDo): Iniciando projeto e adicionando Header e Footer

---
+++ todo_react/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div>Conteudo..</div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
