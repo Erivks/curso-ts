a1785db33bfa174541bbffc37b6bb305aeb0ff23
Erick Santos <erick.santos@planium.io>
2023-08-24 00:07:51 -0300

    feat(toDo): Adicionando tratamento de modal no btn de edição

--- todo_react/src/App.tsx
+++ todo_react/src/App.tsx
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
+import Modal from './components/Modal';
 
 // Style
 import styles from './App.module.css';
@@ -23,8 +24,22 @@ function App() {
     );
   }
 
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal");
+    if (display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  } 
+
+  const editTask = (): void => {
+    hideOrShowModal(true);
+  }
+
   return (
     <div>
+      <Modal children={ <TaskForm btnText="Editar" taskList={taskList}/>}/>
       <Header />
       <main className={styles.main}>
         <div>Conteudo..</div>
@@ -38,7 +53,7 @@ function App() {
         </div>
         <div>
           <h2>Suas tarefas:</h2>
-          <TaskList taskList={taskList} handleDelete={deleteTask}/>
+          <TaskList taskList={taskList} handleDelete={deleteTask} handleEdit={editTask}/>
         </div>
       </main>
       <Footer />
